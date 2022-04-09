686191af035071ff87e8902af3746c7059c2beda
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2022-04-08 22:46:42 -0300

    fixing lint issues

--- feature-home/src/main/java/com/luisfagundes/feature_home/HomeFragment.kt
+++ feature-home/src/main/java/com/luisfagundes/feature_home/HomeFragment.kt
@@ -2,5 +2,4 @@ package com.luisfagundes.feature_home
 
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
-}
+class HomeFragment : Fragment(R.layout.fragment_home)
