30f4af65839a6c3721a27e814f458e42bdfa9c4b
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2022-04-11 12:10:01 -0300

    refactoring recipe mapper

--- data/src/main/java/com/luisfagundes/data/network/mapper/RecipeDetailsMapper.kt
+++ data/src/main/java/com/luisfagundes/data/network/mapper/RecipeDetailsMapper.kt
@@ -1,17 +1,10 @@
 package com.luisfagundes.data.network.mapper
 
 import com.luisfagundes.core.Response
-import com.luisfagundes.data.network.response.IngredientResponse
-import com.luisfagundes.data.network.response.MeasureResponse
-import com.luisfagundes.data.network.response.MeasuresResponse
+import com.luisfagundes.data.network.mapper.IngredientMapper.mapToDomain
 import com.luisfagundes.data.network.response.RecipeResponse
-import com.luisfagundes.domain.model.Ingredient
-import com.luisfagundes.domain.model.Measure
-import com.luisfagundes.domain.model.Measures
 import com.luisfagundes.domain.model.Recipe
 import com.luisfagundes.extensions.empty
-
-private const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
 
 object RecipeDetailsMapper {
     fun Response<RecipeResponse>.mapToDomain(): Response<Recipe> {
@@ -42,33 +35,4 @@ object RecipeDetailsMapper {
                 ingredients = this.data.extendedIngredients?.mapToDomain() ?: emptyList()
             )
         )
-
-    private fun List<IngredientResponse>.mapToDomain(): List<Ingredient> {
-        return this.map { it.toDomain() }
-    }
-
-    private fun IngredientResponse.toDomain(): Ingredient {
-        return Ingredient(
-            id = this.id,
-            amount = this.amount,
-            name = this.name,
-            image = BASE_IMAGE_URL + this.image,
-            measures = this.measures.mapToDomain()
-        )
-    }
-
-    private fun MeasuresResponse.mapToDomain(): Measures {
-        return Measures(
-            metric = this.metric.toDomain(),
-            us = this.us.toDomain()
-        )
-    }
-
-    private fun MeasureResponse.toDomain(): Measure {
-        return Measure(
-            amount = this.amount,
-            unitLong = this.unitLong,
-            unitShort = this.unitShort
-        )
-    }
 }
